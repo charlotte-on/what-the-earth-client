e94b81a044f739e3645a3cf4dcf798bdd312ebc0
Calypso Asline <aslinecalypso@gmail.com>
2021-01-21 16:16:36 +0100

    styling about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const About = () => {
   return (
-    <div style={{ position: "relative" }}>
-      <img
-        src="/media/about.jpeg"
-        alt="vegetables"
+    <div>
+      <div
         style={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          width: "auto",
           height: "100vh",
-          width: "100vw",
+          backgroundImage: "url(/media/about3.jpeg)",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "relative",
         }}
-      />
+      ></div>
 
       <div
         style={{
@@ -26,20 +32,143 @@ const About = () => {
         }}
       >
         <h2>A propos</h2>
-        <br />
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Impact écologique</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Pour ceux qui veulent connaître l'impacte environnemental de leur
+              alimentation et agir en conséquence.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
 
-        <ul>
-          <li>
-            Pour ceux qui veulent connaître l'impacte environnemental de leur
-            alimentation et agir en conséquence.
-          </li>
-          <li>
-            Pour ceux qui ont pris conscience qu'ils pouvaient mieux consommer.
-          </li>
-          <li>Pour ceux qui veulent mieux manger : local, frais, fermier.</li>
-          <li>Pour ceux qui souhaitent acheter en direct des producteurs.</li>
-          <li>Pour ceux qui s'engagent pour un monde meilleur.</li>
-        </ul>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Prise de conscience</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Pour ceux qui ont pris conscience qu'ils pouvaient mieux consommer
+              et manger : local, frais, fermier.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel4a-header"
+          >
+            <Typography>Petits producteurs</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Pour ceux qui souhaitent acheter en direct des producteurs.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel4a-content"
+            id="panel4a-header"
+          >
+            <Typography>Équipe engagée</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <div
+                style={{
+                  border: "1px solid",
+                  borderRadius: "5px",
+                  margin: "10px",
+                  padding: "10px",
+                  backgroundImage: "url(/media/about.jpeg)",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  color: "white",
+                }}
+              >
+                <h4>Victorien Provenzano</h4>
+                <h4>Co-Founder</h4>
+                <div>
+                  <a href="https://github.com/vicpzn">
+                    <i class="fab fa-github"></i>
+                  </a>{" "}
+                  |
+                  <a href="https://www.linkedin.com/in/victorienprovenzano/">
+                    <i class="fab fa-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div
+                style={{
+                  border: "1px solid",
+                  borderRadius: "5px",
+                  margin: "10px",
+                  padding: "10px",
+                  backgroundImage: "url(/media/about2.jpeg)",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  color: "white",
+                }}
+              >
+                <h4>Charlotte Oury</h4>
+                <h4>Co-Founder</h4>
+                <div>
+                  <a href="https://github.com/charlotte-on">
+                    <i class="fab fa-github"></i>
+                  </a>{" "}
+                  |
+                  <a href="https://www.linkedin.com/in/charlotte-oury-8866621a3/">
+                    <i class="fab fa-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+
+              <div
+                style={{
+                  border: "1px solid",
+                  borderRadius: "5px",
+                  margin: "10px",
+                  padding: "10px",
+                  backgroundImage: "url(/media/about.jpeg)",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  color: "white",
+                }}
+              >
+                <h4>Calypso Asline</h4>
+                <h4>Co-Founder</h4>
+                <div>
+                  <a href="https://github.com/Calyaln">
+                    <i class="fab fa-github"></i>
+                  </a>{" "}
+                  |
+                  <a href="https://www.linkedin.com/in/calypso-asline-3b616b78/">
+                    <i class="fab fa-linkedin"></i>
+                  </a>
+                </div>
+              </div>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
